9cbf72b09bb66a9e8824a95acde09565e1137050
Syed Ali Raza Bokhari <isyedaliraza@gmail.com>
2022-08-20 21:26:32 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+// Importing required modules
+const dotenv = require('dotenv')
+const fs = require('fs')
+const cron = require('node-cron')
+const nodemailer = require('nodemailer')
+
+// Configure environment variables
+dotenv.config()
+
+// Create email transporter
+const transporter = nodemailer.createTransport({
+	service: 'gmail',
+	auth: {
+		user: process.env.EMAIL,
+		pass: process.env.PASSWORD,
+	},
+})
+
+// Reading the body of the email from body.html file
+const body = fs.readFileSync('body.html')
+
+// Schedule a cron that will run every Monday to Friday from 9 AM to 6 PM
+cron.schedule('* 9-18 * * 1-5', (_) => {
+	transporter.sendMail(
+		{
+			from: process.env.EMAIL,
+			to: 'recipient@example.com',
+			subject: 'Subject goes here',
+			html: body.toString(),
+		},
+		(err, info) => {
+			if (err) {
+				console.log(err.message)
+				console.log(err.stack)
+			} else {
+				console.log(info.response)
+			}
+		}
+	)
+})
---
+++ readme.md
@@ -0,0 +1,12 @@
+# Email Bot
+A program that will send recurring emails.
+
+## Get Started
+- Install the npm dependencies by running the command `npm install`
+- Create .env file with two keys:
+    - EMAIL (Your gmail email)
+    - PASSWORD (Your gmail less secure app password)
+- Open index.js file and give the cron configuration as per your requirements
+- Run the command `npm start`
+
+© 2022 [Syed Ali Raza Bokhari](https://github.com/isyedaliraza)
